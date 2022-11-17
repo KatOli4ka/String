e770ca2835d4d4878d9ac3d26ec57ccef9d7bf8c
KatOli4ka <katoli4ka87@mail.ru>
2022-11-17 16:01:01 +0500

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println(fullName);
+        System.out.println("ФИО сотрудника - "+fullName);
+
+        System.out.println("Задача 2");
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+upperCaseFullName);
+
     }
 }
